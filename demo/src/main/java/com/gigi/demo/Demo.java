36b023772f8d460894cbb144218e8aa410e74aef
Gloal <alori.glo@gmail.com>
2024-06-11 00:26:26 +0100

    Added try catch to the demo classs getNumber method so that output prints after exception is thrown

--- demo/src/main/java/com/gigi/demo/Demo.java
+++ demo/src/main/java/com/gigi/demo/Demo.java
@@ -2,19 +2,23 @@ package com.gigi.demo;
 
 public class Demo {
 
-    public static int grandParentMethod(int position) throws Exception{
+    public static int grandParentMethod(int position) {
         return parentMethod(position);
     }
 
-    private static int parentMethod(int position) throws Exception{
+    private static int parentMethod(int position){
         return getNumber(position);
     }
 
-    public static int getNumber(int position) throws Exception{
+    public static int getNumber(int position) {
         int output = 0;
 
+        try{
             int[] numbers = new int[] { 1, 2, 3, 4, 5 };
             output = numbers[position];
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return output;
     }
 }

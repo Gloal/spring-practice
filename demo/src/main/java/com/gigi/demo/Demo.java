72bd08c3278b501f85b228e9d4fd566460d4d255
Gloal <alori.glo@gmail.com>
2024-06-11 00:09:32 +0100

    added try-catch block to  the Trial class method call

--- demo/src/main/java/com/gigi/demo/Demo.java
+++ demo/src/main/java/com/gigi/demo/Demo.java
@@ -2,20 +2,19 @@ package com.gigi.demo;
 
 public class Demo {
 
-    public static int grandParentMethod(int position) {
+    public static int grandParentMethod(int position) throws Exception{
         return parentMethod(position);
     }
 
-    private static int parentMethod(int position) {
+    private static int parentMethod(int position) throws Exception{
         return getNumber(position);
     }
 
-    public static int getNumber(int position) {
+    public static int getNumber(int position) throws Exception{
         int output = 0;
 
             int[] numbers = new int[] { 1, 2, 3, 4, 5 };
             output = numbers[position];
-
         return output;
     }
 }
--- demo/src/main/java/com/gigi/demo/Trial.java
+++ demo/src/main/java/com/gigi/demo/Trial.java
@@ -1,10 +1,13 @@
 package com.gigi.demo;
 
 public class Trial {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+        try {
     System.out.println("The value is "+ Demo.grandParentMethod(7));
-
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
